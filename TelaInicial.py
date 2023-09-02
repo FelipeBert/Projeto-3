b330e99263c77f3946a07a55711267b1cb9406e7
Felipe Bertulino <117488841+FelipeBert@users.noreply.github.com>
2023-09-02 15:22:59 -0300

    Combinação dos scripts e ajuste do botão de home.

--- TelaInicial.py
+++ TelaInicial.py
@@ -1,19 +1,352 @@
 import tkinter as tk
 from tkinter import *
-import Selecao
+import subprocess
+import threading
 
 window = tk.Tk()
-width= window.winfo_screenwidth()  
-height= window.winfo_screenheight() 
-window.geometry("%dx%d" % (width, height)) 
-window.title("Shape Motion")
-window.iconbitmap("images/ShapeMotion.ico")
+
+def init(window):
+    width= window.winfo_screenwidth()  
+    height= window.winfo_screenheight() 
+    window.geometry("%dx%d" % (width, height)) 
+    window.title("Shape Motion")
+    window.iconbitmap("images/ShapeMotion.ico")
+    background_img = PhotoImage(file = f"images/background.png")
+    label_background = Label(window, image=background_img).pack()
+
+    textUpChallenger = tk.Label(
+        window,
+        font=("Cabin-Bold", int(18.0)),
+        text="Em Breve",
+        background="#1c1c1c",
+        foreground="#ffffff"
+        )
+    textUpChallenger.place(x=96,y=350)
+
+    textCommunity = tk.Label(
+        window,
+        font=("Cabin-Bold", int(18.0)),
+        text="Comunidade",
+        background="#1c1c1c",
+        foreground="#ffffff"
+        )
+    textCommunity.place(x=96,y=50)
+
+    img0 = PhotoImage(file = f"images/img0.png")
+    b0 = Button(
+        image = img0,
+        borderwidth = 0,
+        highlightthickness = 0,
+        command = btn_clicked,
+        relief = "flat")
+
+    b0.place(
+        x = 20, y = 175,
+        width = 28.5,
+        height = 27)
+
+    img1 = PhotoImage(file = f"images/img1.png")
+    b1 = Button(
+        image = img1,
+        borderwidth = 0,
+        highlightthickness = 0,
+        command = btn_clicked,
+        relief = "flat")
+
+    b1.place(
+        x = 7, y = 670,
+        width=45,
+        height=30
+        )
+
+    img2 = PhotoImage(file = f"images/img2.png")
+    b2 = Button(
+        image = img2,
+        borderwidth = 0,
+        highlightthickness = 0,
+        command = btn_clicked,
+        relief = "flat")
+
+    b2.place(
+        x = 19, y = 97,
+        width = 28,
+        height = 28)
+
+    img3 = PhotoImage(file = f"images/img3.png")
+    b3 = Button(
+        image = img3,
+        borderwidth = 0,
+        highlightthickness = 0,
+        command = lambda: btn_clicked_home(window),
+        relief = "flat")
+
+    b3.place(
+        x = 20, y = 22,
+        width = 28,
+        height = 28)
+
+    img4 = PhotoImage(file = f"images/img4.png")
+    b4 = Button(
+        image = img4,
+        borderwidth = 0,
+        highlightthickness = 0,
+        command = btn_clicked_img4,
+        relief = "flat")
+
+    b4.place(
+        x = 67,
+        y = 96,
+        width=300,
+        height=200
+    )
+
+    img5 = PhotoImage(file = f"images/img5.png")
+    b5 = Button(
+        image = img5,
+        borderwidth = 0,
+        highlightthickness = 0,
+        command = btn_clicked,
+        relief = "flat")
+
+    b5.place(
+        x = 1754, y = 103,
+        width = 86,
+        height = 86)
+
+    img6 = PhotoImage(file = f"images/img6.png")
+    b6 = Button(
+        image = img6,
+        borderwidth = 0,
+        highlightthickness = 0,
+        command = btn_clicked,
+        relief = "flat")
+
+    b6.place(
+        x = 1773, y = 733,
+        width = 55,
+        height = 39)
+
+    botoes = [b0, b1, b2, b3, b4, b5, b6]
+
+    for botao in botoes:
+        botao.bind("<Enter>", destacar_botao)
+        botao.bind("<Leave>", remover_destaque_botao)
+
+    window.mainloop()
 
 def btn_clicked():
     print("Botão Clicado")
 
 def btn_clicked_img4():
-    Selecao.init(window)
+    selecao(window)
+
+def btn_clicked_home(window):
+    for widget in window.winfo_children():
+        widget.destroy()
+    init(window)
+
+def selecao(window):
+    for widget in window.winfo_children():
+        widget.destroy()
+
+    width= window.winfo_screenwidth()  
+    height= window.winfo_screenheight() 
+    window.geometry("%dx%d" % (width, height)) 
+    window.title("Shape Motion")
+    window.iconbitmap("images/ShapeMotion.ico")
+
+    background2_img = PhotoImage(file = f"images/background2.png")
+    label_background2 = Label(window, image=background2_img).pack()
+
+    textSelecExer = tk.Label(
+        window,
+        font=("Cabin-Bold", int(25.0)),
+        text="Selecine seu exercicio:",
+        background="#1c1c1c",
+        foreground="#ffffff"
+        )
+    textSelecExer.place(x=96,y=50)
+
+    img0 = PhotoImage(file = f"images/img9.png")
+    b0 = Button(
+        image = img0,
+        borderwidth = 0,
+        highlightthickness = 0,
+        command = lambda: btn_clicked_home(window),
+        relief = "flat")
+
+    b0.place(
+        x = 20, y = 20,
+        width = 28,
+        height = 28)
+
+    img1 = PhotoImage(file = f"images/img10.png")
+    b1 = Button(
+        image = img1,
+        borderwidth = 0,
+        highlightthickness = 0,
+        command = btn_clicked,
+        relief = "flat")
+
+    b1.place(
+        x = 19, y = 103,
+        width = 28,
+        height = 28)
+
+    img2 = PhotoImage(file = f"images/img11.png")
+    b2 = Button(
+        image = img2,
+        borderwidth = 0,
+        highlightthickness = 0,
+        command = btn_clicked,
+        relief = "flat")
+
+    b2.place(
+        x = 20, y = 184,
+        width = 27,
+        height = 27)
+
+    img3 = PhotoImage(file = f"images/img12.png")
+    b3 = Button(
+        image = img3,
+        borderwidth = 0,
+        highlightthickness = 0,
+        command = btn_clicked,
+        relief = "flat")
+
+    b3.place(
+        x = 7, y = 670,
+        width=45,
+        height=30)
+
+    img4 = PhotoImage(file = f"images/img13.png")
+    b4 = Button(
+        image = img4,
+        borderwidth = 0,
+        highlightthickness = 0,
+        command = btn_clicked_img13,
+        relief = "flat")
+
+    b4.place(
+        x = 500, y = 184,
+        width = 300,
+        height = 200)
+
+    img5 = PhotoImage(file = f"images/img14.png")
+    b5 = Button(
+        image = img5,
+        borderwidth = 0,
+        highlightthickness = 0,
+        command = btn_clicked_img14,
+        relief = "flat")
+
+    b5.place(
+        x = 114, y = 184,
+        width = 300,
+        height = 200)
+
+    img6 = PhotoImage(file = f"images/img15.png")
+    b6 = Button(
+        image = img6,
+        borderwidth = 0,
+        highlightthickness = 0,
+        command = btn_clicked_img15,
+        relief = "flat")
+
+    b6.place(
+        x = 886, y = 184,
+        width = 300,
+        height = 200)
+
+    img7 = PhotoImage(file = f"images/img7.png")
+    b7 = Button(
+        image = img7,
+        borderwidth = 0,
+        highlightthickness = 0,
+        command = btn_clicked_img7,
+        relief = "flat")
+
+    b7.place(
+        x = 114, y = 394,
+        width = 300,
+        height = 200)
+
+    img8 = PhotoImage(file = f"images/img8.png")
+    b8 = Button(
+        image = img8,
+        borderwidth = 0,
+        highlightthickness = 0,
+        command = btn_clicked_img8,
+        relief = "flat")
+
+    b8.place(
+        x = 500, y = 394,
+        width = 300,
+        height = 200)
+
+    botoes = [b0, b1, b2, b3, b4, b5, b6, b7, b8]
+
+    for botao in botoes:
+        botao.bind("<Enter>", destacar_botao)
+        botao.bind("<Leave>", remover_destaque_botao)
+
+    #window.resizable(False, False)
+    window.mainloop()
+
+def btn_clicked_img15():
+    def run_script():
+        exercise_type = "push-up"
+        root_value = "root_value"
+        subprocess.call(["python", "main.py", exercise_type, root_value])
+
+    thread = threading.Thread(target=run_script)
+    thread.start()
+
+    window.destroy()
+
+def btn_clicked_img14():
+    def run_script():
+        exercise_type = "sit-up"
+        root_value = "root_value"
+        subprocess.call(["python", "main.py", exercise_type, root_value])
+
+    thread = threading.Thread(target=run_script)
+    thread.start()
+
+    window.destroy()
+
+def btn_clicked_img13():
+    def run_script():
+        exercise_type = "squat"
+        root_value = "root_value"
+        subprocess.call(["python", "main.py", exercise_type, root_value])
+
+    thread = threading.Thread(target=run_script)
+    thread.start()
+
+    window.destroy()
+
+def btn_clicked_img7():
+    def run_script():
+        exercise_type = "walk"
+        root_value = "root_value"
+        subprocess.call(["python", "main.py", exercise_type, root_value])
+
+    thread = threading.Thread(target=run_script)
+    thread.start()
+
+    window.destroy()
+
+def btn_clicked_img8():
+    def run_script():
+        exercise_type = "pull-up"
+        root_value = "root_value"
+        subprocess.call(["python", "main.py", exercise_type, root_value])
+
+    thread = threading.Thread(target=run_script)
+    thread.start()
+
+    window.destroy()
 
 def destacar_botao(event):
     event.widget.config(bg="gray", cursor="hand2")
@@ -21,125 +354,4 @@ def destacar_botao(event):
 def remover_destaque_botao(event):
     event.widget.config(bg="SystemButtonFace", cursor="arrow")
 
-background_img = PhotoImage(file = f"images/background.png")
-label_background = Label(window, image=background_img).pack()
-
-textUpChallenger = tk.Label(
-    window,
-    font=("Cabin-Bold", int(18.0)),
-    text="Em Breve",
-    background="#1c1c1c",
-    foreground="#ffffff"
-    )
-textUpChallenger.place(x=96,y=350)
-
-textCommunity = tk.Label(
-    window,
-    font=("Cabin-Bold", int(18.0)),
-    text="Comunidade",
-    background="#1c1c1c",
-    foreground="#ffffff"
-    )
-textCommunity.place(x=96,y=50)
-
-img0 = PhotoImage(file = f"images/img0.png")
-b0 = Button(
-    image = img0,
-    borderwidth = 0,
-    highlightthickness = 0,
-    command = btn_clicked,
-    relief = "flat")
-
-b0.place(
-    x = 20, y = 175,
-    width = 28.5,
-    height = 27)
-
-img1 = PhotoImage(file = f"images/img1.png")
-b1 = Button(
-    image = img1,
-    borderwidth = 0,
-    highlightthickness = 0,
-    command = btn_clicked,
-    relief = "flat")
-
-b1.place(
-    x = 7, y = 670,
-    width=45,
-    height=30
-    )
-
-img2 = PhotoImage(file = f"images/img2.png")
-b2 = Button(
-    image = img2,
-    borderwidth = 0,
-    highlightthickness = 0,
-    command = btn_clicked,
-    relief = "flat")
-
-b2.place(
-    x = 19, y = 97,
-    width = 28,
-    height = 28)
-
-img3 = PhotoImage(file = f"images/img3.png")
-b3 = Button(
-    image = img3,
-    borderwidth = 0,
-    highlightthickness = 0,
-    command = btn_clicked,
-    relief = "flat")
-
-b3.place(
-    x = 20, y = 22,
-    width = 28,
-    height = 28)
-
-img4 = PhotoImage(file = f"images/img4.png")
-b4 = Button(
-    image = img4,
-    borderwidth = 0,
-    highlightthickness = 0,
-    command = btn_clicked_img4,
-    relief = "flat")
-
-b4.place(
-    x = 67,
-    y = 96,
-    width=300,
-    height=200
-)
-
-img5 = PhotoImage(file = f"images/img5.png")
-b5 = Button(
-    image = img5,
-    borderwidth = 0,
-    highlightthickness = 0,
-    command = btn_clicked,
-    relief = "flat")
-
-b5.place(
-    x = 1754, y = 103,
-    width = 86,
-    height = 86)
-
-img6 = PhotoImage(file = f"images/img6.png")
-b6 = Button(
-    image = img6,
-    borderwidth = 0,
-    highlightthickness = 0,
-    command = btn_clicked,
-    relief = "flat")
-
-b6.place(
-    x = 1773, y = 733,
-    width = 55,
-    height = 39)
-
-botoes = [b0, b1, b2, b3, b4, b5, b6]
-
-for botao in botoes:
-    botao.bind("<Enter>", destacar_botao)
-    botao.bind("<Leave>", remover_destaque_botao)
-
-window.mainloop()
+init(window)
